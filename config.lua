4a800bdff9bb9df82c80f91b65b1e4bf02574473
James Adam <34058513+sudo-jaa@users.noreply.github.com>
2022-08-25 10:28:49 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,8 @@ lvim.format_on_save = false
 vim.o.termguicolors = true;
 
 lvim.colorscheme = "material"
+vim.g.sonokai_style = "atlantis"
+
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
@@ -141,10 +143,23 @@ lvim.plugins = {
     "marko-cerovac/material.nvim",
     config = function()
       require("material").setup({
-        custom_highlights = {}
+        custom_highlights = {},
+        contrast = {
+          sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+          floating_windows = true, -- Enable contrast for floating windows
+          line_numbers = true, -- Enable contrast background for line numbers
+          sign_column = true, -- Enable contrast background for the sign column
+          cursor_line = true, -- Enable darker background for the cursor line
+          non_current_windows = true, -- Enable darker background for non-current windows
+          popup_menu = true, -- Enable lighter background for the popup menu
+        },
       })
     end
   },
+  {
+    "sainnhe/sonokai",
+  },
+  { "sainnhe/gruvbox-material" },
   { "glepnir/oceanic-material" },
   { "mhartington/oceanic-next" },
   { "xolox/vim-misc" },
@@ -210,8 +225,11 @@ lvim.plugins = {
   {
     "prettier/vim-prettier"
   },
+  -- {
+  --   "ggandor/lightspeed.nvim"
+  -- },
   {
-    "ggandor/lightspeed.nvim"
+    "ggandor/leap.nvim"
   },
   {
     "folke/todo-comments.nvim",
@@ -224,28 +242,28 @@ lvim.plugins = {
       }
     end
   },
-  --   {
-  --   "simrat39/rust-tools.nvim",
-  --   config = function()
-  --     local lsp_installer_servers = require "nvim-lsp-installer.servers"
-  --     local _, requested_server = lsp_installer_servers.get_server "rust_analyzer"
-  --     require("rust-tools").setup({
-  --       tools = {
-  --         autoSetHints = true,
-  --         hover_with_actions = true,
-  --         runnables = {
-  --           use_telescope = true,
-  --         },
-  --       },
-  --       server = {
-  --         cmd_env = requested_server._default_options.cmd_env,
-  --         on_attach = require("lvim.lsp").common_on_attach,
-  --         on_init = require("lvim.lsp").common_on_init,
-  --       },
-  --     })
-  --   end,
-  --   ft = { "rust", "rs" },
-  -- },
+  {
+    "simrat39/rust-tools.nvim",
+    config = function()
+      local lsp_installer_servers = require "nvim-lsp-installer.servers"
+      local _, requested_server = lsp_installer_servers.get_server "rust_analyzer"
+      require("rust-tools").setup({
+        tools = {
+          autoSetHints = true,
+          hover_with_actions = true,
+          runnables = {
+            use_telescope = true,
+          },
+        },
+        server = {
+          cmd_env = requested_server._default_options.cmd_env,
+          on_attach = require("lvim.lsp").common_on_attach,
+          on_init = require("lvim.lsp").common_on_init,
+        },
+      })
+    end,
+    ft = { "rust", "rs" },
+  },
   {
     "rcarriga/nvim-dap-ui"
   },
@@ -266,8 +284,149 @@ lvim.plugins = {
   },
   {
     'liuchengxu/vista.vim'
+  },
+  {
+    "axelvc/template-string.nvim"
+  },
+  {
+    'ldelossa/gh.nvim',
+    requires = { { 'ldelossa/litee.nvim' } }
+  },
+  {
+    "mcchrish/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    requires = "rktjmp/lush.nvim"
+  },
+  { "rebelot/kanagawa.nvim" },
+  -- {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'},
+  {
+    "danymat/neogen",
+    config = function()
+      require('neogen').setup {}
+    end,
   }
 }
+
+require('neogen').setup {
+  enabled = true, --if you want to disable Neogen
+  input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+  -- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
+}
+
+require('leap').set_default_keymaps()
+
+-- vim.o.foldcolumn = '1'
+-- vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+-- vim.o.foldlevelstart = 99
+-- vim.o.foldenable = true
+
+-- -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+-- vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+-- vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+-- require('ufo').setup({
+--     provider_selector = function(bufnr, filetype, buftype)
+--         return {'treesitter', 'indent'}
+--     end
+-- })
+
+require('litee.lib').setup()
+require('litee.gh').setup({
+  -- deprecated, around for compatability for now.
+  jump_mode             = "invoking",
+  -- remap the arrow keys to resize any litee.nvim windows.
+  map_resize_keys       = false,
+  -- do not map any keys inside any gh.nvim buffers.
+  disable_keymaps       = false,
+  -- the icon set to use.
+  icon_set              = "default",
+  -- any custom icons to use.
+  icon_set_custom       = nil,
+  -- whether to register the @username and #issue_number omnifunc completion
+  -- in buffers which start with .git/
+  git_buffer_completion = true,
+  -- defines keymaps in gh.nvim buffers.
+  keymaps               = {
+    -- when inside a gh.nvim panel, this key will open a node if it has
+    -- any futher functionality. for example, hitting <CR> on a commit node
+    -- will open the commit's changed files in a new gh.nvim panel.
+    open = "<CR>",
+    -- when inside a gh.nvim panel, expand a collapsed node
+    expand = "zo",
+    -- when inside a gh.nvim panel, collpased and expanded node
+    collapse = "zc",
+    -- when cursor is over a "#1234" formatted issue or PR, open its details
+    -- and comments in a new tab.
+    goto_issue = "gd",
+    -- show any details about a node, typically, this reveals commit messages
+    -- and submitted review bodys.
+    details = "d",
+    -- inside a convo buffer, submit a comment
+    submit_comment = "<C-s>",
+    -- inside a convo buffer, when your cursor is ontop of a comment, open
+    -- up a set of actions that can be performed.
+    actions = "<C-a>",
+    -- inside a thread convo buffer, resolve the thread.
+    resolve_thread = "<C-r>",
+    -- inside a gh.nvim panel, if possible, open the node's web URL in your
+    -- browser. useful particularily for digging into external failed CI
+    -- checks.
+    goto_web = "gx"
+  }
+})
+
+lvim.builtin.which_key.mappings["G"] = {
+  name = "+Github",
+  c = {
+    name = "+Commits",
+    c = { "<cmd>GHCloseCommit<cr>", "Close" },
+    e = { "<cmd>GHExpandCommit<cr>", "Expand" },
+    o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+    p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+    z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+  },
+  i = {
+    name = "+Issues",
+    p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+  },
+  l = {
+    name = "+Litee",
+    t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+  },
+  r = {
+    name = "+Review",
+    b = { "<cmd>GHStartReview<cr>", "Begin" },
+    c = { "<cmd>GHCloseReview<cr>", "Close" },
+    d = { "<cmd>GHDeleteReview<cr>", "Delete" },
+    e = { "<cmd>GHExpandReview<cr>", "Expand" },
+    s = { "<cmd>GHSubmitReview<cr>", "Submit" },
+    z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+  },
+  p = {
+    name = "+Pull Request",
+    c = { "<cmd>GHClosePR<cr>", "Close" },
+    d = { "<cmd>GHPRDetails<cr>", "Details" },
+    e = { "<cmd>GHExpandPR<cr>", "Expand" },
+    o = { "<cmd>GHOpenPR<cr>", "Open" },
+    p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+    r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+    t = { "<cmd>GHOpenToPR<cr>", "Open To" },
+    z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+  },
+  t = {
+    name = "+Threads",
+    c = { "<cmd>GHCreateThread<cr>", "Create" },
+    n = { "<cmd>GHNextThread<cr>", "Next" },
+    t = { "<cmd>GHToggleThread<cr>", "Toggle" },
+  },
+}
+
+require('template-string').setup({
+  filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' }, -- filetypes where the plugin is active
+  jsx_brackets = true, -- should add brackets to jsx attributes
+})
 
 require('telescope').setup({
   pickers = {
@@ -278,9 +437,9 @@ require('telescope').setup({
 })
 
 require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
+  -- for example, context is off by default, use this to turn it on
+  show_current_context = true,
+  show_current_context_start = true,
 }
 
 lvim.builtin.dap.active = true
